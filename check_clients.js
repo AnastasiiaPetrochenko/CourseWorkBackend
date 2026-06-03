a0077d9b14b24d3e7955b98eedce5c00ecda1c20
AnastasiiaPetrochenko <anastasiia.petrochenko.pz.2023@lpnu.ua>
2026-06-03 21:39:07 +0300

    feat: configure environment variables for database connection and add .gitignore file

--- check_clients.js
+++ check_clients.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const { Client } = require('pg');
-const client = new Client({ user: 'postgres', password: '221804Nn', host: 'localhost', port: 5432, database: 'my_cinema_db' });
+const client = new Client({ user: process.env.DB_USER, password: process.env.DB_PASSWORD, host: process.env.DB_HOST, port: process.env.DB_PORT, database: process.env.DB_NAME });
 async function run() {
   await client.connect();
   let res = await client.query(`SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'clients'`);
